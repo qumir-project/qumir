953633338b536c3f18e7b1dd497a64d37bb4eeed
m <proyectoqumir@gmail.com>
2021-04-11 18:38:39 -0400

    starting new project

--- README.md
+++
@@ -1,3 +0,0 @@
-# Little serverless app for plants management
-
-## Working rendirng data [2021/03/24]
--- js/mainReact.js
+++
@@ -1,5 +0,0 @@
-/*
-<div class="w3-row" id="m1"></div>
-        <div class="w3-row" id="m2"></div>
-        <div class="w3-row" id="m3"></div>
-*/
